d7229917f9e82c156c78c6d23f3fdd29ad1c24bb
Atul-cloud <80030624+Atul-cloud@users.noreply.github.com>
2022-04-10 22:08:40 +0530

    Update multiplicationtable.py

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import math
 import os
 import random
@@ -8,7 +10,9 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
-    if n>=1 and n<=10:
-        for i in range(1, 11):
-            print(n,"x",i,"=",n * i)
+    for i in range(1, 11):
+        print(n,"x",i,"=",n * i)
+            
       
+            
+        
